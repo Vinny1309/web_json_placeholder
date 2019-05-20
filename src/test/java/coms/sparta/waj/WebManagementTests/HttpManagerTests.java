11854a559a7dac4965e6df9f499aa8ae0cb1417c
Waine James <wjames@spartaglobal.com>
2019-05-20 14:35:34 +0100

    Create http manager unit tests.

--- src/test/java/coms/sparta/waj/WebManagementTests/HttpManagerTests.java
+++ src/test/java/coms/sparta/waj/WebManagementTests/HttpManagerTests.java
@@ -1,10 +1,45 @@
 package coms.sparta.waj.WebManagementTests;
 
+import coms.sparta.waj.Utility.PropertiesReader;
 import coms.sparta.waj.webmanagement.HttpManager;
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.Test;
 
 public class HttpManagerTests
 {
-    HttpManager manager;
+    private static HttpManager manager;
+    private static PropertiesReader propertiesReader;
+
+    @BeforeClass
+    public static void setUp()
+    {
+        manager = new HttpManager();
+        propertiesReader = new PropertiesReader();
+    }
+
+    //regex match checks that the string is bounded by square brackets. i.e. that
+    //an array is returned.
+    @Test
+    public void testUsersRetrieval()
+    {
+        String returnString = manager.getAllUserJson();
+        System.out.println(returnString);
+        Assert.assertTrue(returnString.matches("\\u005b[\\s\\S]*\\u005d"));
+    }
+
+    @Test
+    public void testSingleUserRetrieval()
+    {
+        String returnString = manager.getSingleUserJson();
+        System.out.println(returnString);
+        Assert.assertTrue(returnString.matches("\\u007b[\\s\\S]*\\u007d"));
+    }
+
+
+
+
+
 
 
 }
